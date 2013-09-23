13d415b9c9b176bd55da3b7caab01e0dfd8aed30
MeowMixer <meowmixer@meowmix-ThinkPad-X220.(none)>
2013-09-22 23:42:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ColladaTest::Application.config.secret_key_base = '24edb0c041d2f292374085d4743d4143d0aca3b7c6aeba7fd44fa321b44b105ac5d44f1e306b2d6232214d7db18149feb835a24d0748e1b4578d27e8f3c38117'
